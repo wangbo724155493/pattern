d99ce2b78a01f7ca51f3875157ec3c6ab66923ef
wangbo <72415545564>
2019-11-22 16:30:48 +0800

    提交分支test

---
+++ pattern/src/main/java/com/wb/pattern/adapter/Member.java
@@ -0,0 +1,44 @@
+package com.wb.pattern.adapter;
+
+/**
+ * @author wangbo阿斯顿发射点旅客发送量看风景ask了解对方阿斯蒂芬啊舍得离开房间ask劳动纠纷啊士大夫立刻就阿桑
+ * @since 2019/11/6 14:25顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶
+ */
+public class Member {
+    private String username;
+    private String password;
+    private String mid;
+    private String info;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
